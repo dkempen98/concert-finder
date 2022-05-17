84e0881200032260ba2786fea24b96d68720f92f
Drew Kempen <drewkempen98@gmail.com>
2022-05-17 13:57:24 -0400

    Add time converter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
+var concertDate = "5/17/2022";
+var weatherDate;
 
+function getWeatherDate() {
+  weatherDate = Math.floor(new Date(concertDate).getTime() / 1000);
+  console.log(weatherDate);
+}
+
+getWeatherDate();
