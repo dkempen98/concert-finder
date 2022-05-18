4933f82875a32a0249ab4a54ad76880cff3d3bb7
Drew Kempen <drewkempen98@gmail.com>
2022-05-18 12:50:11 -0400

    days from concert function

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,22 @@ function getWeatherDate() {
   weatherDate = Math.floor(new Date(concertDate).getTime() / 1000);
 }
 
+function weatherAvailable() {
+  // 86400000 is equivelant to one day based on the way js calculates numbers
+  concertDate = new Date(2022, 4, 19);
+  var daysUntilConcert;
+  var today = new Date();
+  daysUntilConcert = (concertDate.getTime() - today.getTime()) / 86400000;
+  daysUntilConcert = Math.ceil(daysUntilConcert);
+  if (daysUntilConcert > 8) {
+    console.log("n/a");
+    return;
+  } else {
+    // call weather function
+    console.log(daysUntilConcert);
+  }
+}
+
 function calculateCovidScore() {
   // The formula is avgWeeklyCases/5 = x => currentWeekTotal / x => score
 
@@ -32,10 +48,12 @@ function calculateCovidScore() {
 function weeksSincePandemicStart() {
   // Start of pandemic, gave some leeway to avoid using the few weeks of very
   // low weekly numbers from the first couple weeks
-  var startDate = new Date(2020, 04, 01);
+  var startDate = new Date(2020, 03, 01);
   var curDate = new Date();
   var timeBetween = (curDate.getTime() - startDate.getTime()) / 1000;
   timeBetween = timeBetween / (60 * 60 * 24 * 7);
   console.log(timeBetween);
   return timeBetween;
 }
+
+weatherAvailable();
