d4a9669ceb5a8e2a95c454f67b7d78d1deaded3a
Drew Kempen <drewkempen98@gmail.com>
2022-05-19 12:56:14 -0400

    Add weather functionality improvements

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,4 +1,3 @@
-
 var name;
 var local_names;
 var lat;
@@ -6,92 +5,85 @@ var lon;
 var state;
 var country;
 
-var formSubmitHandler = function(weather) {
-    weather.preventDefault();
-    
-    var name = nameInputEl.value.trim();
-    
-    if (name) {
-        getWeather(weather)
-            weatherInputEl.value = '';
-    }
+var geoData;
+var cityComp;
+
+var formSubmitHandler = function (weather) {
+  weather.preventDefault();
+
+  var name = nameInputEl.value.trim();
+
+  if (name) {
+    getWeather(weather);
+    weatherInputEl.value = "";
+  }
 };
 
 // OpenWeather Weather API
-    // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-        //&exclude is optional
+// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+//&exclude is optional
 
-
-        https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid=ed7ad3517cceea24f5ddf34da86b3e00
-
+//        https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid=ed7ad3517cceea24f5ddf34da86b3e00
 
 // OpenWeather Direct Geocoding API
-    // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-        // &limit is optional
-    
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+// &limit is optional
 
-        http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=ed7ad3517cceea24f5ddf34da86b3e00
+//        http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=ed7ad3517cceea24f5ddf34da86b3e00
 
-        var getWeather = function(weather) {
-            var weatherApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + ed7ad3517cceea24f5ddf34da86b3e00
-            
-            fetch(weatherApi)
-                .then(function (data) {
-                    console.log(data);
-                    var name = data.name;
-                    var local_names = data.local_names.en;
-                    var lat = data.lat;
-                    var lon = data.lon;
-                    var state = data.state;
-                    var country = data.country;
-                                       
-                });
+var getWeather = function (weather) {
+  var weatherApi =
+    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+    // lat +
+    "39.9812" +
+    "&lon=" +
+    // lon +
+    "82.9988" +
+    "&units=imperial&appid=ed7ad3517cceea24f5ddf34da86b3e00";
 
-var name;
-var local_names;
-var lat;
-var lon;
-var state;
-var country;
+  fetch(weatherApi)
+    .then(function (response) {
+      if (!response.ok) {
+        throw alert("Error retrieving data, please try again");
+      }
 
-var formSubmitHandler = function(weather) {
-    weather.preventDefault();
-    
-    var name = nameInputEl.value.trim();
-    
-    if (name) {
-        getWeather(weather)
-            weatherInputEl.value = '';
-    }
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      getWeatherReport(data);
+    });
 };
+function getWeatherReport(weatherData) {
+  console.log(weatherData);
+  var weatherDesc = weatherData.daily[0].weather[0].description;
+  weatherDesc = weatherDesc.toLowerCase();
+  var temp = weatherData.daily[0].temp.eve;
+  // add temp and weatherDesc to the cards
+}
 
-// OpenWeather Weather API
-    // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-        //&exclude is optional
+// var getCity = function () {
+//   var geoApi =
+//     "http://api.openweathermap.org/geo/1.0/direct?q=" +
+//     // cityComp +
+//     "New York" +
+//     "&appid=ed7ad3517cceea24f5ddf34da86b3e00";
 
+//   fetch(geoApi)
+//     .then(function (response) {
+//       if (!response.ok) {
+//         throw alert("Error retrieving data, please try again");
+//       }
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       lon = data.lon;
+//       lat = data.lat;
+//       cityComp = data.name;
+//       console.log(cityComp);
+//       console.log(lon);
+//       console.log(lat);
+//     });
+// };
 
-        https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid=ed7ad3517cceea24f5ddf34da86b3e00
-
-
-// OpenWeather Direct Geocoding API
-    // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-        // &limit is optional
-    
-
-        http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=ed7ad3517cceea24f5ddf34da86b3e00
-
-        var getWeather = function(weather) {
-            var weatherApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + ed7ad3517cceea24f5ddf34da86b3e00
-            
-            fetch(weatherApi)
-                .then(function (data) {
-                    console.log(data);
-                    var name = data.name;
-                    var local_names = data.local_names.en;
-                    var lat = data.lat;
-                    var lon = data.lon;
-                    var state = data.state;
-                    var country = data.country;
-                                       
-                });
-        };
+getWeather();
